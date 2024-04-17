aefa26a0f00607761500740d182f11c9ae93b8cb
mmaybei <yebbi2@ewhain.net>
2024-04-18 00:02:49 +0900

    Initial Commit

---
+++ 34th-Sopt-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  34th-Sopt-Assignment
+//
+//  Created by 예삐 on 4/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
